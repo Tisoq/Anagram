0038c0a7f8062604d183b0b3a88a6972fa9079ff
Lipovetskyy Denys <lipov0etsky1denis@gmail.com>
2022-08-01 12:43:01 +0300

    UI testing

--- app/src/test/java/anagrams/app/AnagramTest.java
+++ app/src/test/java/anagrams/app/AnagramTest.java
@@ -4,8 +4,27 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AnagramTest {
+
+    /**
+     * when (filter) and (input) are empty.
+     */
+
     @Test
-    public void inputReversingWithEmptyFilter() {
+    public void Empty_Input_And_Empty_Filter() {
+        String input = "";
+        String filter = "";
+
+        String expected = " ";
+
+        assertEquals(expected, Anagram.build(input, filter));
+    }
+
+    /**
+     * when (filter) is empty and (input) consist String.
+     */
+
+    @Test
+    public void input_Reversing_With_Empty_Filter() {
         String input = "Foxminded cool 24/7";
         String filter = "";
 
@@ -14,8 +33,12 @@ public class AnagramTest {
         assertEquals(expected, Anagram.build(input, filter));
     }
 
+    /**
+     * when String are in input, but not in filter.
+     */
+
     @Test
-    public void inputReversingWithoutFilterCharacterInInput() {
+    public void input_Reversing_Without_Filter_Character_In_Input() {
         String input = "abcd efgh";
         String filter = "0123456789";
 
@@ -24,8 +47,12 @@ public class AnagramTest {
         assertEquals(expected, Anagram.build(input, filter));
     }
 
+    /**
+     * when String in input, and simultaneously String in filter.
+     */
+
     @Test
-    public void inputReversingWithFilterCharacterInInput() {
+    public void input_Reversing_With_Filter_Character_In_Input() {
         String input = "4uzzia is a te44orist sta7e";
         String filter = "0123456789";
 
@@ -33,4 +60,5 @@ public class AnagramTest {
 
         assertEquals(expected, Anagram.build(input, filter));
     }
+
 }
